a1a8812645e28912492be914718a311550e35245
arshamalh <arshamalh@gmail.com>
2022-01-03 19:03:44 +0330

    Characters Collection added.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 import { Sorter } from "./Sorter";
 
 const sorter = new Sorter(new NumbersCollection([10, 0, -5, 3, 7, -2]));
 sorter.sort();
+const sorter2 = new Sorter(new CharactersCollection("Xaayb"));
+sorter2.sort();
 console.log(sorter.collection);
+console.log(sorter2.collection);
