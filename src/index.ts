8742f820218eb18eee534e5127da3a724ecbaba6
arshamalh <arshamalh@gmail.com>
2022-01-19 18:30:47 +0330

    Added abstract class.

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
 import { Sorter } from "./Sorter";
 
-const sorter = new Sorter(new NumbersCollection([10, 0, -5, 3, 7, -2]));
-sorter.sort();
-const sorter2 = new Sorter(new CharactersCollection("Xaayb"));
-sorter2.sort();
-console.log(sorter.collection);
-console.log(sorter2.collection);
+const numbers = new NumbersCollection([10, 0, -5, 3, 7, -2]);
+numbers.sort();
+const characters = new CharactersCollection("Xaayb");
+characters.sort();
+console.log(numbers);
+console.log(characters);
