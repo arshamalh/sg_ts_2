154aba4112f56e8d4b2e2f42b24178201e4cef1e
arshamalh <arshamalh@gmail.com>
2022-01-19 19:00:04 +0330

    linked list added.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
-import { Sorter } from "./Sorter";
+import { LinkedList } from "./LinkedList";
+// LinkedList
 
 const numbers = new NumbersCollection([10, 0, -5, 3, 7, -2]);
 numbers.sort();
@@ -8,3 +9,12 @@ const characters = new CharactersCollection("Xaayb");
 characters.sort();
 console.log(numbers);
 console.log(characters);
+
+const liked_list = new LinkedList();
+liked_list.add(3);
+liked_list.add(7);
+liked_list.add(2);
+liked_list.add(-2);
+
+liked_list.sort();
+liked_list.print();
